21bcc96de311e31f0cf6c0d4c41f60dd9fc0dbfe
Dirk Watteroth <dirk.watteroth@googlemail.com>
2018-04-16 15:20:56 +0200

    first operations in operation decoder

--- PicSimulator/PicSimulator/Microcontroller/OperationDecoder.cs
+++ PicSimulator/PicSimulator/Microcontroller/OperationDecoder.cs
@@ -10,6 +10,27 @@ namespace PicSimulator.Microcontroller
     {
         public static OperationsEnum DecodeOperation(int binaryOperationCode)
         {
+            if(binaryOperationCode == 0)
+            {
+                return OperationsEnum.NOP;
+            }
+
+            //byte oriented operations
+            var operationCode6bit = binaryOperationCode & 16128;
+            var operationCode4bit = binaryOperationCode & 15360;
+            if(operationCode6bit == 1792)
+            {
+                return OperationsEnum.ADDWF;
+            }
+
+            //bit oriented operations
+
+            //literal and control operations
+            if(operationCode4bit == 12288)
+            {
+                return OperationsEnum.MOVLW;
+            }
+
             return OperationsEnum.UNKNOWN;
         }
     }
